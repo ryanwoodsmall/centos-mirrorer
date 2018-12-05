46f973e78be4cc49ba96afad91a1ec68c5d8dfbd
ryan woodsmall <rwoodsmall@gmail.com>
2018-12-05 18:55:37 +0000

    rpm-repo-counts.sh: bail if config file not found

--- bin/rpm-repo-counts.sh
+++ bin/rpm-repo-counts.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-CONFFILE="$(dirname ${0})/../conf/centos-mirrorer.env"
-source "${CONFFILE}"
+CONFFILE="$(dirname $(realpath ${0}))/../conf/centos-mirrorer.env"
+if [ ! -e "${CONFFILE}" ] ; then
+  echo "${CONFFILE} not found"
+  exit 1
+else
+  source "${CONFFILE}"
+fi
 
 # XXX - need to build this from ${REPOLIST}
 for i in ${REPOBASE}/*/*/*/*/ ; do
