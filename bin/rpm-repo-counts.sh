843dec355aa605bcf3557e1bd04af1d6a6f58f7e
ryan woodsmall <rwoodsmall@gmail.com>
2018-12-06 08:32:36 +0000

    use bash array and string splitting instead of subshell command/output replacement

--- bin/rpm-repo-counts.sh
+++ bin/rpm-repo-counts.sh
@@ -4,19 +4,19 @@ CONFFILE="$(dirname $(realpath ${0}))/../conf/centos-mirrorer.env"
 if [ ! -e "${CONFFILE}" ] ; then
   echo "${CONFFILE} not found"
   exit 1
-else
-  source "${CONFFILE}"
 fi
-
-REPOLIST="$(echo ${REPOLIST} | tr \  \\n | cut -f1-4 -d: | tr : / | sed "s#^#${REPOBASE}/#g" | sed 's#$#/#g' | xargs echo)"
+source "${CONFFILE}"
 
 for i in ${REPOLIST} ; do
-  echo "${i}"
-  r="$(basename ${i})"
-  a="$(basename $(dirname ${i}))"
-  v="$(basename $(dirname $(dirname ${i})))"
-  o="$(basename $(dirname $(dirname $(dirname ${i}))))"
+  i="${i%:*}"
+  d="${REPOBASE}/${i//://}"
+  echo "${d}"
+  i=( $(echo "${i}" | tr : '\n') )
+  o="${i[0]}"
+  v="${i[1]}"
+  a="${i[2]}"
+  r="${i[3]}"
   f="/tmp/${o}-${v}-${a}-${r}.out"
-  find "${i}" -type f -name \*.rpm | awk -F/ '{print $NF}' | sort > "${f}"
+  find "${d}" -type f -name \*.rpm | awk -F/ '{print $NF}' | sort > "${f}"
   echo "${f} $(cat ${f} | wc -l) $(cat ${f} | sort -u | wc -l)"
 done
